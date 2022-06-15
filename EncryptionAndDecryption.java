05acb9801d0201095e0e6379c435d71133466e03
Raaz53 <81346299+Raaz53@users.noreply.github.com>
2022-06-15 17:23:11 +0545

    Add files via upload

---
+++ EncryptionAndDecryption.java
@@ -0,0 +1,64 @@
+
+
+public class EncryptionAndDecryption {
+    
+    public static String encrypt(String message, int key){
+        char[] chars = message.toCharArray();
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char[] temp = new char[chars.length];
+        int position=0, non_position;
+        // System.out.println(alphabet);
+ 
+        for(int i = 0; i<chars.length; i++){
+            for (int j=0; j<alphabet.length; j++){
+                if(chars[i]==alphabet[j]){
+                    position = j;
+                    break;
+                }
+            }
+
+            non_position = position+key;
+            if(non_position>alphabet.length-2){
+                non_position %= alphabet.length;
+            }
+            temp[i] = alphabet[non_position];
+        }
+        String cipher = String.valueOf(temp);
+        return cipher;
+    }
+
+    public static String decryption(String encoded, int key){
+        char[] chars = encoded.toCharArray();
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char[] temp = new char[chars.length];
+        int position=0, non_position;
+        // System.out.println(alphabet);
+ 
+        for(int i = 0; i<chars.length; i++){
+            for (int j=0; j<alphabet.length; j++){
+                if(chars[i]==alphabet[j]){
+                    position = j;
+                    break;
+                }
+            }
+
+            non_position = position-key;
+            if(non_position<key){
+                non_position -= alphabet.length-1;
+            }
+            temp[i] = alphabet[non_position];
+        }
+        String decoded = String.valueOf(temp);
+        return decoded;
+    }
+    public static void main(String[] args) {
+        String text = "hellothere";
+        int key = 4;
+        String encrypted = encrypt(text, key);
+        String decrypted = decryption(encrypted, key);
+
+        System.out.println(encrypted);
+        System.out.println(decrypted);
+    }
+    
+}
