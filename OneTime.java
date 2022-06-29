e067e1fdaf90f6b9d29ea1b097e3575420d9b452
Raaz53 <81346299+Raaz53@users.noreply.github.com>
2022-06-29 06:20:56 +0545

    Add files via upload

--- OneTime.java
+++ OneTime.java
@@ -1,26 +1,66 @@
+// public class OneTime {
+//     private static String encrypt(String message, String key) {
+//         char[] chars = message.toCharArray();
+//         char[] keyword = key.toCharArray();
+//         for( int i = 0; i< chars.length; i++) {
+//             chars[i] -= keyword[i];
+//         }
+//         return String.valueOf(chars);
+//     }
+//     private static String decrypt(String cipher, String key){
+//         char[] chars = cipher.toCharArray();
+//         char[] keyword = key.toCharArray();
+//         for( int i = 0; i< chars.length; i++) {
+//             chars[i] += keyword[i];
+//         }
+//         return String.valueOf(chars);
+//     }
+//     public static void main(String[] args) {
+//         String message = "abvd";
+//         String key ="erds";
+//         String cipher = encrypt(message, key);
+//         String decryption = decrypt(cipher, key);
+//         System.out.println(cipher);
+//         System.out.println(decryption);
+//     }
+// }
+
+
 public class OneTime {
-    private static String enrypt(String message, String key) {
-        char[] chars = message.toCharArray();
-        char[] keyword = key.toCharArray();
-        for( int i = 0; i< chars.length; i++) {
-            chars[i] -= keyword[i];
+    public static String encrypt(int message, String key){
+        String messageBinary = Integer.toBinaryString(message);
+        if(messageBinary.length()<key.length()){
+            for(int i = 0; i<(key.length()-messageBinary.length()); i++ ) {
+                messageBinary = 0 +messageBinary;
+            }
         }
-        return String.valueOf(chars);
+        if(messageBinary.length()>key.length()){
+            for(int i = 0; i<(messageBinary.length()-key.length()); i++ ) {
+                key = 0 +key;
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < messageBinary.length(); i++) {
+            sb.append(key.charAt(i)^messageBinary.charAt(i));
+        }
+        return String.valueOf(sb);
     }
-    private static String decrypt(String cipher, String key){
-        char[] chars = cipher.toCharArray();
-        char[] keyword = key.toCharArray();
-        for( int i = 0; i< chars.length; i++) {
-            chars[i] += keyword[i];
+    public static int decrypt(String cipher, String key) {
+        
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < cipher.length(); i++) {
+            sb.append(key.charAt(i)^cipher.charAt(i));
         }
-        return String.valueOf(chars);
+        cipher = String.valueOf(sb);
+        return Integer.parseInt(cipher, 2);
     }
     public static void main(String[] args) {
-        String message = "~0137BSA";
-        String key ="8DHA0!`~";
-        String cipher = enrypt(message, key);
-        String decryption = decrypt(cipher, key);
-        System.out.println(cipher);
-        System.out.println(decryption);
+        int message = 17;
+        int key = 16;
+        String keyBinary = Integer.toBinaryString(key);
+        System.out.println(encrypt(message, keyBinary));
+        System.out.println(decrypt(encrypt(message, keyBinary), keyBinary));
+
     }
+    
 }
