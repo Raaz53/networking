5012599819661ef86be648945d5fa2c2b72664fe
Raaz53 <81346299+Raaz53@users.noreply.github.com>
2022-06-29 12:03:52 +0545

    Update Question10.java

--- Question10.java
+++ Question10.java
@@ -1,19 +1,19 @@
 public class Question10 {
-    public static int indexOf(int[] arr, int element) {
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] == element){
-                return i;
-            }
-        }
-        return -1;
-    }
+    public static boolean isPrime(int n) {  
+        if (n <= 1)  
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {  
+            if (n % i == 0) 
+                return false;   
+        }  
+        return true;  
+    }  
     public static void main(String[] args) {
         int[] arr = {10,12,17,19,25,85,96,56,11};
-        int ele1 = 17;
-        int ele2 = 19;
-        int ele3 = 11;
-        System.out.println(ele1 + " - At index " +indexOf(arr, ele1));
-        System.out.println(ele2 + " - At index " +indexOf(arr, ele2));
-        System.out.println(ele3 + " - At index " +indexOf(arr, ele3));
+        for(int i = 0; i<arr.length; i++) {
+            if(isPrime(arr[i])) {
+                System.out.println(arr[i] + " - At index " + i);
+            }
+        }
     }
 }
